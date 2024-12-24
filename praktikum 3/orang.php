d0daad2c4251677401f6a053e33df27beef3bf7b
likhul <likhulhadi141@gmail.com>
2024-12-24 20:33:54 +0700

    Update orang.php

--- praktikum 3/orang.php
+++ praktikum 3/orang.php
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
 <?php
 class orang{
     protected $nama;
     public function ucapsalam(){
         echo "Hallo Perkenalkan Nama Saya" . $this->nama . "<br>";
     }
-=======
 <?php
 class orang{
     protected $nama;
     public function ucapsalam(){
         echo "Hallo Perkenalkan Nama Saya" . $this->nama . "<br>";
     }
->>>>>>> 8e1c37434798e7578aaf1566da87db10a1d028cc
 }
